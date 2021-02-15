00547b38ac13052d0d62305b9a66a54910fbcaeb
せいい <superstarzsw@gmail.com>
2021-02-16 01:57:55 +0900

    feat: 整理逻辑中

--- bin/just-react.js
+++ bin/just-react.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const program = require("commander");
+const program = require('commander');
 program
-  .version(require("../package").version)
-  .usage("<command> [options]")
-  .command("init", "create a new react project.");
+    .version(require('../package').version)
+    .usage('<command> [options]')
+    .command('init', 'create a new react project.');
 program.parse(process.argv);
